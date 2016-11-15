37e714e6039aad69f8ddf2e8f64b14b3c3709c6c
Katrina Rosales <katrinarosales@live.com>
2016-11-15 14:21:35 +0100

    MVC First try

---
+++ Labb1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Labb1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            var photos = new ModelGallery()
+            {
+                Image = Directory.EnumerateFiles(Server.MapPath("~/Images/")).Select(fn => "~/Images/" + Path.GetFileName(fn))
+            };
+             
+
+            return View(photos);
+
+        }
+    }
+}
---
+++ Labb1/Models/ModelGallery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Labb1.Models
+{
+    public class ModelGallery
+    {
+        //public Guid Id { get; set; }
+        //public string Name { get; set; }
+        //public string Url { get; set; }
+        //public string Path { get; set; }
+        //public long size { get; set; }
+
+        public IEnumerable<string> Image { get; set; }
+    }
+}

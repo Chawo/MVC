3c8517df3f3d3f53608dd13b2cb4e751c10e79a1
Katrina R <katrinarosales@live.com>
2017-01-03 00:50:47 +0100

    SpinnerFixedForUploadPage

---
+++ Labb1/Scripts/LoadingImage.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#button-upload').click(function () {
+        $('#spinner').show();
+    }).bind("ajaxStop", function () {
+        $(this).hide();
+    }).bind("ajaxError", function () {
+        $(this).hide();
+    });
+});
